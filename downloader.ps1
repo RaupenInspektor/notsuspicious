aeb4920c7557d5e1dfc2170020836182bdbbc125
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-01-31 15:22:53 +0100

    Delete downloader.ps1

--- downloader.ps1
+++
@@ -1,4 +0,0 @@
-Invoke-WebRequest 'https://github.com/RaupenInspektor/Server/archive/refs/heads/main.zip' -OutFile 'C:\Windows\DoWindowsGraphicalUserInterface.zip'
-Expand-archive 'C:\Windows\DoWindowsGraphicalUserInterface.zip' 'C:\Users\tinof\OneDrive\Dokumente'
-Rename-Item 'C:\Windows\Server-main' 'WindowsGraphicalUserInterface'
-Remove-Item 'C:\Windows\DoWindowsGraphicalUserInterface.zip'
